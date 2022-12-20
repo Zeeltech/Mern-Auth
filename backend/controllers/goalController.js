da9c5fde4e7460c4b4b563703493f9f73bf16504
Zeel <zeeldesai2163@gmail.com>
2022-12-20 09:45:18 +0530

    JWT and fully authorization

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,13 +3,14 @@ const asyncHandler = require("express-async-handler");
 //hve asyncHandler thi wrap kri daiye async function ne
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json({ goals });
 });
@@ -28,6 +29,7 @@ const setGoals = asyncHandler(async (req, res) => {
   //text chhe e attribute levanu postman body mathi
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json({ goal });
@@ -44,7 +46,20 @@ const updateGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
+  //STARTS FOR AUTH
+  const user = await User.findById(req.user.id);
+  //Check if user request
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
 
+  //Make sure logged in user matches goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not matched");
+  }
+  //END FOR AUTH
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body);
 
   res.status(200).json({ updatedGoal });
@@ -60,6 +75,20 @@ const deleteGoals = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  //Check if user request
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  //Make sure logged in user matches goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not matched");
   }
 
   await goal.remove();
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 const {
   getGoals,
   setGoals,
@@ -8,7 +9,7 @@ const {
 } = require("../controllers/goalController");
 
 //short version of roouter.get .....
-router.route("/").get(getGoals).post(setGoals);
-router.route("/:id").put(updateGoals).delete(deleteGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
+router.route("/:id").put(protect, updateGoals).delete(protect,deleteGoals);
 
 module.exports = router;
