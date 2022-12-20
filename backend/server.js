5a8dd61e723d0be1772d2581538a59c24e9e3633
Zeel <zeeldesai2163@gmail.com>
2022-12-20 08:12:33 +0530

    userController userModel userRoute and all 2 methods of userController

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const router = require("./routes/goalRoutes");
+const userrouter = require("./routes/userRoutes");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", router);
+app.use("/api/users", userrouter);
 
 app.use(errorHandler);
 
