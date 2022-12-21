626491e630987118773941aeb50ec569b2a59c1b
Zeel <zeeldesai2163@gmail.com>
2022-12-21 21:02:31 +0530

    Frontend with Register dummy

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"; //slash fa is for font-awsome library only
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      {/* In index.css header class ma justify content space-between chhe etle bey left and right khune rhe chhe GoalSetter and Login,Register(UL) */}
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt />
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUser />
+            Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
